d22b7671eb41556c12c39d339e2235090107dd0f
Robert Batusek <robert.batusek@gmail.com>
2024-08-08 16:31:18 +0200

    Python VS Code setup added

---
+++ python/firsttest/mainpagetest.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class MainPageTest(unittest.TestCase):
+    def test_simple_automated_test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
