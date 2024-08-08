c9599fb1041f1b03cdae1501a2380aa13edad2de
Robert Batusek <robert.batusek@gmail.com>
2024-08-08 17:02:06 +0200

    The first running test (outside of container)

--- python/firsttest/mainpagetest.py
+++ python/firsttest/mainpagetest.py
@@ -1,9 +1,19 @@
 import unittest
+from playwright.sync_api import sync_playwright
 
 
-class MainPageTest(unittest.TestCase):
+class OpenWeatherMapMainPageTest(unittest.TestCase):
+
     def test_simple_automated_test(self):
-        self.assertTrue(True)
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
 
+            print("opening page")
+            page.goto("https://openweathermap.org/")
+            page.wait_for_load_state("networkidle")
+            
+            self.assertEqual(page.title(),"Current weather and forecast")
+        
 if __name__ == '__main__':
     unittest.main()
