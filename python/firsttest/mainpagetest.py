e3b1f4ec52ec9d78832572ac02eb0d6d84302679
Robert Batusek <robert.batusek@gmail.com>
2024-08-17 11:11:03 +0200

    Tests rewritten to OpenStreetMap

--- python/firsttest/mainpagetest.py
+++ python/firsttest/mainpagetest.py
@@ -2,41 +2,53 @@ import unittest
 from playwright.sync_api import sync_playwright
 
 
-# TODO: make it work in a container
-
 class OpenWeatherMapMainPageTest(unittest.TestCase):
 
     def test_simple_automated_test(self):
         with sync_playwright() as p:
-            # TODO: make it work with head browser
+            # runs browser in a headless mode
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
-            page.goto("https://openweathermap.org/")
+            page.goto("https://openstreetmap.org/")
             page.wait_for_load_state("networkidle")
             
-            self.assertEqual(page.title(),"Current weather and forecast")
+            map = page.locator("#map")
+            self.assertIsNotNone(map)
+
+    def test_explore_locators(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+
+            page.goto("https://openstreetmap.org/")
+            page.wait_for_load_state("networkidle")
+            
+            heading = page.get_by_role("heading", name="OpenStreetMap")
+            self.assertIsNotNone(heading)
+
+            search = page.get_by_text("History")
+            self.assertEqual(search.get_attribute("id"),"history_tab")
 
     def test_temperature_displayed_after_save(self):
         with sync_playwright() as p:
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
-            page.goto("https://openweathermap.org/")
+            page.goto("https://openstreetmap.org/")
             page.wait_for_load_state("networkidle")
             
-            search_bar = page.get_by_placeholder("Search city")
+            search_bar = page.get_by_role("textbox", name="Search")
             search_bar.fill("Prague")
 
-            search_button = page.locator("button[type='submit']")
+            search_button = page.get_by_role("button", name="Go")
             search_button.click()
 
             # Wait for the search results to load (we will deal with timeouts later)
-            page.wait_for_timeout(2000)
+            page.wait_for_timeout(1000)
 
-            # Verify the current temperature is displayed
-            current_temp = page.locator('ul.search-dropdown-menu li span:nth-of-type(2)')
-            self.assertNotEqual(current_temp,"")
+            results = page.locator('#sidebar_content')
+            self.assertNotEqual(results.text_content,"")
 
 
 if __name__ == '__main__':
