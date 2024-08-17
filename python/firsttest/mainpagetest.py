0d46eee1d9a21c4ac461c8890a932c2a6c37a734
Robert Batusek <robert.batusek@gmail.com>
2024-08-17 08:13:11 +0200

    A test added that clicks the search button and verifies the result.

--- python/firsttest/mainpagetest.py
+++ python/firsttest/mainpagetest.py
@@ -9,11 +9,33 @@ class OpenWeatherMapMainPageTest(unittest.TestCase):
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
-            print("opening page")
             page.goto("https://openweathermap.org/")
             page.wait_for_load_state("networkidle")
             
             self.assertEqual(page.title(),"Current weather and forecast")
-        
+
+    def test_temperature_displayed_after_save(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+
+            page.goto("https://openweathermap.org/")
+            page.wait_for_load_state("networkidle")
+            
+            search_bar = page.get_by_placeholder("Search city")
+            search_bar.fill("Prague")
+
+            # Click the search button
+            search_button = page.locator("button[type='submit']")
+            search_button.click()
+
+            # Wait for the search results to load (adjust timeout as needed)
+            page.wait_for_timeout(2000)
+
+            # Verify the current temperature is displayed
+            current_temp = page.locator('ul.search-dropdown-menu li span:nth-of-type(2)')
+            self.assertNotEqual(current_temp,"")
+
+
 if __name__ == '__main__':
     unittest.main()
