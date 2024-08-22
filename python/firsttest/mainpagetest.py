13c8d66eef46a89c18fa942ceca2bec0c4302ffd
Robert Batusek <robert.batusek@gmail.com>
2024-08-22 10:51:26 +0200

    Common setup extracted

--- python/firsttest/mainpagetest.py
+++ python/firsttest/mainpagetest.py
@@ -3,52 +3,41 @@ from playwright.sync_api import sync_playwright
 
 
 class OpenWeatherMapMainPageTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        playwright = sync_playwright()
+        p = playwright.start()
+        
+        # runs browser in a headless mode
+        browser = p.chromium.launch(headless=True)
+        cls.page = browser.new_page()
+        
+    def setUp(self):
+        self.page.goto("https://openstreetmap.org/")
+        self.page.wait_for_load_state("networkidle")
 
     def test_simple_automated_test(self):
-        with sync_playwright() as p:
-            # runs browser in a headless mode
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
-
-            page.goto("https://openstreetmap.org/")
-            page.wait_for_load_state("networkidle")
-            
-            map = page.locator("#map")
-            self.assertIsNotNone(map)
+        map = self.page.locator("#map")
+        self.assertIsNotNone(map)
 
     def test_explore_locators(self):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
+        heading = self.page.get_by_role("heading", name="OpenStreetMap")
+        self.assertIsNotNone(heading)
 
-            page.goto("https://openstreetmap.org/")
-            page.wait_for_load_state("networkidle")
-            
-            heading = page.get_by_role("heading", name="OpenStreetMap")
-            self.assertIsNotNone(heading)
-
-            search = page.get_by_text("History")
-            self.assertEqual(search.get_attribute("id"),"history_tab")
+        search = self.page.get_by_text("History")
+        self.assertEqual(search.get_attribute("id"),"history_tab")
 
     def test_temperature_displayed_after_save(self):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
+        search_bar = self.page.get_by_role("textbox", name="Search")
+        search_bar.fill("Prague")
+        search_button = self.page.get_by_role("button", name="Go")
+        search_button.click()
 
-            page.goto("https://openstreetmap.org/")
-            page.wait_for_load_state("networkidle")
-            
-            search_bar = page.get_by_role("textbox", name="Search")
-            search_bar.fill("Prague")
+        # Wait for the search results to load (we will deal with timeouts later)
+        self.page.wait_for_timeout(1000)
 
-            search_button = page.get_by_role("button", name="Go")
-            search_button.click()
-
-            # Wait for the search results to load (we will deal with timeouts later)
-            page.wait_for_timeout(1000)
-
-            results = page.locator('#sidebar_content')
-            self.assertNotEqual(results.text_content,"")
+        results = self.page.locator('#sidebar_content')
+        self.assertNotEqual(results.text_content,"")
 
 
 if __name__ == '__main__':
