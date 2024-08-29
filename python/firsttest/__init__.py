634a8bbc833649569147d1c6e36d032fbb1d8484
Robert Batusek <robert.batusek@gmail.com>
2024-08-29 10:45:01 +0200

    Parallel run demonstrated on two tests

---
+++ python/firsttest/__init__.py
@@ -0,0 +1,1 @@
+from .mainpagetest import OpenWeatherMapMainPageTest, AnotherTest
--- python/firsttest/mainpagetest.py
+++ python/firsttest/mainpagetest.py
@@ -1,4 +1,6 @@
+import time
 import unittest
+import multiprocessing
 from playwright.sync_api import sync_playwright
 
 
@@ -23,6 +25,7 @@ class OpenWeatherMapMainPageTest(unittest.TestCase):
     def test_explore_locators(self):
         heading = self.page.get_by_role("heading", name="OpenStreetMap")
         self.assertIsNotNone(heading)
+        self.page.wait_for_timeout(2000)
 
         search = self.page.get_by_text("History")
         self.assertEqual(search.get_attribute("id"),"history_tab")
@@ -40,5 +43,19 @@ class OpenWeatherMapMainPageTest(unittest.TestCase):
         self.assertNotEqual(results.text_content,"")
 
 
+class AnotherTest(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+        time.sleep(2)
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+        time.sleep(2)
+
+
+    
 if __name__ == '__main__':
     unittest.main()
+    # run in sequence with "python -m unittest"
+    # run in parallel with "unittest-parallel --level class"
