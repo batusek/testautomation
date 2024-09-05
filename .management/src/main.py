def1ab481ac40be7289031c79515bd11edce8abc
Robert Batusek <robert.batusek@gmail.com>
2024-09-05 11:32:34 +0200

    Exercises 2 and 3 generated

--- .management/src/main.py
+++ .management/src/main.py
@@ -1,3 +1,5 @@
+import shutil
+
 def removeSolution(filename: str, start: int, end: int):
     with open(filename, "r") as f:
         lines = f.readlines()
@@ -11,3 +13,6 @@ def removeSolution(filename: str, start: int, end: int):
                 f.write(line)
 
 removeSolution("../python/practice1_intro/practice1.py", 10,15)
+shutil.copy("../python/practice2_locators/practice2.py", "../python/practice3_setup/practice3.py")
+shutil.copy("../python/practice1_intro/practice1.py", "../python/practice2_locators/practice2.py")
+
