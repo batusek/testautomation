d30ddc99f47fd0378b11d96f951ff0da6c78df2d
Robert Batusek <robert.batusek@gmail.com>
2024-09-07 07:16:38 +0200

    More configuration examples

--- javascript/practice4_parallel/playwright.config.example.ts
+++ javascript/practice4_parallel/playwright.config.example.ts
@@ -22,6 +22,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* Sets 30s timeout for each test */
+  timeout: 30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
