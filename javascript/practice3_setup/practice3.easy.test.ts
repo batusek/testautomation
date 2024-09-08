6c85e7a1b1c18f0e0340751e87073b19be28702e
Robert Batusek <robert.batusek@gmail.com>
2024-09-08 08:18:16 +0200

    Second exercise for setup prepared

---
+++ javascript/practice3_setup/practice3.easy.test.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+test('search returns results', async( {page} ) => {
+  await page.goto('https://openstreetmap.org/');
+    
+  const search_bar = await page.getByRole("textbox", { name:"Search"} );
+  search_bar.fill("Olomouc");
+
+  const search_button = await page.getByRole("button", { name: "Go"} );
+  search_button.click();
+
+  const results = await page.locator('#sidebar_content');
+  await expect(results).not.toBeEmpty();
+  await expect(results.textContent).toBeDefined();
+});
+
+test('search changes url', async( {page} ) => {
+  await page.goto('https://openstreetmap.org/');
+    
+  const search_bar = await page.getByRole("textbox", { name:"Search"} );
+  search_bar.fill("Olomouc");
+
+  const search_button = await page.getByRole("button", { name: "Go"} );
+  search_button.click();
+
+  await page.locator('#sidebar_content');
+  await page.waitForLoadState('domcontentloaded');
+  await expect(page.url()).toContain("49.59");
+});
