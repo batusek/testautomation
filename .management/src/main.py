3b60a079bc5e4776a84ec867b729a026d0eb6732
Robert Batusek <robert.batusek@gmail.com>
2024-09-08 07:46:53 +0200

    Javascript exercises 3,4,5 generated

--- .management/src/main.py
+++ .management/src/main.py
@@ -46,10 +46,13 @@ def javaScript():
     insertLines(exercise3,10,excerpt)
     insertLines(exercise3,3,excerpt)
 
-    removeLines("../javascript/practice3_setup/playwright.config.ts", 22, 23)
-    removeLines("../javascript/practice3_setup/playwright.config.ts", 20, 21)
-    removeLines("../javascript/practice3_setup/playwright.config.ts", 16, 17)
+    removeLines("../javascript/practice3_setup/playwright.config.ts", 22, 22)
+    removeLines("../javascript/practice3_setup/playwright.config.ts", 20, 20)
+    removeLines("../javascript/practice3_setup/playwright.config.ts", 16, 16)
     removeLines("../javascript/practice3_setup/setup.ts", 3, 16)
+
+    shutil.copy("../javascript/practice1_intro/playwright.config.ts", "../javascript/practice4_parallel/playwright.config.ts")
+    shutil.copy("../javascript/practice1_intro/playwright.config.ts", "../javascript/practice5_retry/playwright.config.ts")
 
 # python()
 javaScript()
