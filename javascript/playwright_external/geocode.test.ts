85347c2266a6ca00bb0064e04c911673a69c1ac4
Robert Batusek <robert.batusek@gmail.com>
2025-03-08 06:26:11 +0100

    Refactoring - geocode turned into a class

--- javascript/playwright_external/geocode.test.ts
+++ javascript/playwright_external/geocode.test.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
-import { geocode } from './geocode';
+import { Geocoder } from './geocode';
 
 test('geocode Blansko', async ({ page }) => {
     let city = "Blansko";
-    let coordinates = await geocode(city);
+    const geocoder = new Geocoder();
+    let coordinates = await geocoder.geocode(city);
     const [latitude, longitude] = coordinates;
     expect(latitude).toBeCloseTo(49.36,2);
     expect(longitude).toBeCloseTo(16.64,2);
