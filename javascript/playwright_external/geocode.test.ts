32400ca6b33eb8f89db66c2ebb421c15a9d88894
Robert Batusek <robert.batusek@gmail.com>
2025-03-08 06:52:47 +0100

    Test for Znojmo added

--- javascript/playwright_external/geocode.test.ts
+++ javascript/playwright_external/geocode.test.ts
@@ -2,8 +2,12 @@ import { test, expect } from '@playwright/test';
 import { Geocoder, GeocoderSimulator } from './geocode';
 
 // After start
-test.describe('Geocoder', () => {
-    const geocoders = [new Geocoder(), new GeocoderSimulator({ "Blansko": [49.36, 16.64] })];
+test.describe('Geocoder tests', () => {
+    const geocoders = [new Geocoder(), new GeocoderSimulator({ 
+        "Blansko": [49.36, 16.64],
+        "Znojmo": [48.86, 16.05]
+    })];
+
     geocoders.forEach(geocoder => {
         test(`geocode Blansko using ${geocoder.constructor.name}`, async () => {
             let coordinates = await geocoder.geocode("Blansko");
@@ -11,7 +15,14 @@ test.describe('Geocoder', () => {
             expect(latitude).toBeCloseTo(49.36,2);
             expect(longitude).toBeCloseTo(16.64,2);
         });
-        
+
+        test(`geocode Znojmo using ${geocoder.constructor.name}`, async () => {
+            let coordinates = await geocoder.geocode("Znojmo");
+            const [latitude, longitude] = coordinates;
+            expect(latitude).toBeCloseTo(48.86, 2);
+            expect(longitude).toBeCloseTo(16.05, 2);
+        });    
+
     });
 });
 
