5d6078124499835536f5ad8a92a03235c6675571
Robert Batusek <robert.batusek@gmail.com>
2025-04-28 15:16:16 +0200

    Table with examples implemented

--- javascript/cucumber_intro/features/step_definitions/statement.ts
+++ javascript/cucumber_intro/features/step_definitions/statement.ts
@@ -1,18 +1,24 @@
 import * as assert from 'assert';
 import { Given, When, Then } from '@cucumber/cucumber';
 
-Given('I have received statement by {string}', function (mode) {
+Given('account with statement distribution by {string}', function (mode) {
     this.modeBefore = mode;
     return 'none';
 });
 
-When('I change the statement from {string} to {string}', function (oldMode, newMode) {
-    assert.strictEqual(this.modeBefore, oldMode);
-    this.modeAfter = newMode;
+When('I change the distribution to {string}', function (mode) {
+    if (mode==='post')
+        this.modeAfter=this.modeBefore;
+    else
+        this.modeAfter = mode;
     return 'none';
 });
 
 Then('the statement should be sent by {string}', function (mode) {
     assert.strictEqual(this.modeAfter, mode);
     return 'none';
+});
+
+Then('error message is {string}', function (error) {
+    return 'pending';
 });
