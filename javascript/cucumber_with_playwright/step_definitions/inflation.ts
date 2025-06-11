088227e9323bfcdf9474e3e3989655f5a562dde1
Robert Batusek <robert.batusek@gmail.com>
2025-06-11 19:22:00 +0200

    Sample solution deleted to keep only the puzzle

--- javascript/cucumber_with_playwright/step_definitions/inflation.ts
+++ javascript/cucumber_with_playwright/step_definitions/inflation.ts
@@ -6,26 +6,17 @@ Given('I have savings of {string} CZK', function (savings) {
 });
 
 Given('the inflation rate is {string} percent', function (inflation) {
-  this.inflation = inflation;
+  // TODO: not implemented
 });
 
 Given('I keep them {string} years', function (years) {
-    this.years = years;
+  // TODO: not implemented
 })
 
-When('I calculate the impact of inflation on my savings', async function () {
-    await this.page.goto('https://www.csas.cz/cs/osobni-finance/investovani?calculation=inflation');
-    await this.page.getByRole('button', { name: 'Souhlasím a pokračovat' }).click();
-    await this.page.getByTestId('RAemInvestmentCalculator--savings-value-input--input').fill(this.savings);
-    await this.page.getByTestId('RAemInvestmentCalculator--years-input--input').fill(this.years);
-    await this.page.getByTestId('RAemInvestmentCalculator--inflation-input--input').fill(this.inflation);
+When('I calculate the impact of inflation on my savings', async function () {7
+  // TODO: not implemented
 });
 
 Then('value of my savings after all those years is {string}', async function (result) {
-  const actual = await this.page.getByTestId("RAemInvestmentCalculator--result-savings-after");
-  actual.click();  // have the value recalculated 
-  await this.page.waitForTimeout(1000);
-  let resultText = await actual.textContent(); 
-  resultText = resultText?.replace(/\u00A0/g, ''); // Remove non-breaking spaces
-  expect(resultText?.trim()).toContain(result);
+  // TODO: not implemented
 });
