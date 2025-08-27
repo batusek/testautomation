555c84c0cf35a5d8f0d322398f83d85f1b6c0500
Robert Batusek <robert.batusek@gmail.com>
2025-08-27 11:15:37 +0200

    Statement feature rewritten to playwright-bdd

--- javascript/playwright_bdd/playwright.config.ts
+++ javascript/playwright_bdd/playwright.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
 const testDir = defineBddConfig({
-  features: 'sample.feature',
-  steps: 'sample.ts',
+  features: 'statement.feature',
+  steps: '*.ts',
 });
 
 export default defineConfig({
