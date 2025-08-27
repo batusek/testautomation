de2b4b6966f66942680c1e7598dcd05d443c1ea5
Robert Batusek <robert.batusek@gmail.com>
2025-08-27 11:16:58 +0200

    Multiple tests run at once

--- javascript/playwright_bdd/playwright.config.ts
+++ javascript/playwright_bdd/playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
 const testDir = defineBddConfig({
-  features: 'statement.feature',
+  features: '*.feature',
   steps: '*.ts',
 });
 
