2bf1d401076a68d1ae8b837bfaa7d4ed78d01b75
Robert Batusek <robert.batusek@gmail.com>
2025-12-02 10:50:44 +0100

    The second dotnet test added

---
+++ .samples/dotnet/playwright_locators/LocatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace PlaywrightLocators;
+
+[TestClass]
+public class LocatorTests : PageTest
+{
+    [TestMethod]
+    public async Task ExploreLocators()
+    {
+        await Page.GotoAsync("https://openstreetmap.org/");
+        
+        // After start
+        var heading = Page.GetByRole(AriaRole.Heading, new() { Name = "OpenStreetMap logo" });
+        await Expect(heading).ToBeVisibleAsync();
+
+        var search = Page.GetByRole(AriaRole.Link, new() { Name = "History" });
+        await Expect(search).ToHaveAttributeAsync("href", "/history");
+        // After end
+    }
+
+    [TestMethod]
+    public async Task SearchReturnsResults()
+    {
+        await Page.GotoAsync("https://openstreetmap.org/");
+        
+        // After start
+        // var search_bar = Page.Locator("#query");
+        var searchBar = Page.GetByRole(AriaRole.Textbox, new() { Name = "Search" });
+        await searchBar.FillAsync("Prague");
+
+        var searchButton = Page.GetByRole(AriaRole.Button, new() { Name = "Go" });
+        await searchButton.ClickAsync();
+
+        var results = Page.Locator("#sidebar_content");
+        await Expect(results).Not.ToBeEmptyAsync();
+        var textContent = await results.TextContentAsync();
+        Assert.IsNotNull(textContent);
+        // After end
+    }
+}
