9f8ad9d72ff58d8d933ada2c4c48711189f6f679
Robert Batusek <robert.batusek@gmail.com>
2025-12-12 13:55:59 +0100

    fix: inflation calculator test

--- .samples/javascript/playwright_bdd/steps/inflation.ts
+++ .samples/javascript/playwright_bdd/steps/inflation.ts
@@ -15,14 +15,14 @@ Given('I keep them {string} years', async ( {world} , years) => {
 
 When('I calculate the impact of inflation on my savings', async ( {world}) => {
   await world.page.goto('https://www.csas.cz/cs/osobni-finance/investovani?calculation=inflation');
-  await world.page.getByRole('button', { name: 'Souhlasit s cookies a pokrač' }).click();
-  await world.page.getByTestId('RAemInvestmentCalculator--savings-value-input--input').fill(world.savings);
-  await world.page.getByTestId('RAemInvestmentCalculator--years-input--input').fill(world.years);
-  await world.page.getByTestId('RAemInvestmentCalculator--inflation-input--input').fill(world.inflation);
+  await world.page.getByRole('button', { name: 'cookies', exact: true }).click();
+  await world.page.getByTestId('inflation-savings--input').fill(world.savings);
+  await world.page.getByTestId('inflation-years--input').fill(world.years);
+  await world.page.getByTestId('inflation-inflation-value--input').fill(world.inflation);
 });
 
 Then('value of my savings after all those years is {string}', async ( {world} , result) => {
-  const actual = await world.page.getByTestId("RAemInvestmentCalculator--result-savings-after");
+  const actual = await world.page.getByTestId("resultAfterInflation");
   actual.click();  // have the value recalculated 
   await world.page.waitForTimeout(1000);
   let resultText = await actual.textContent(); 
